a90c1527e380eb53c2107881fe9094f1ba852408
Runar <runar.johannessen96rgmail.com>
2017-03-09 15:23:05 +0100

    fikset mappeoppsett (?)

--- Conditions.java
+++ Conditions.java
@@ -1,14 +1,24 @@
 package trening;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class Conditions implements ActiveDomainObject{
 	
 	private Exercise exercise;
-	private int temperature;
+	private int id, temperature;
 	private String weather;
 	
 	public Conditions(Exercise exercise, int temp, String weather) {
+		this.id = -1;
+		this.exercise = exercise;
+		this.temperature = temp;
+		this.weather = weather;
+	}
+	
+	public Conditions(int id, Exercise exercise, int temp, String weather) {
+		this.id = id;
 		this.exercise = exercise;
 		this.temperature = temp;
 		this.weather = weather;
@@ -28,7 +38,19 @@ public class Conditions implements ActiveDomainObject{
 
 	@Override
 	public void save(Connection conn) {
-		// TODO Auto-generated method stub
+		try {
+			Statement stmt = conn.createStatement();
+			if (id != -1){
+				stmt.executeUpdate("UPDATE conditions SET exercise="+exercise.getId()+", temperature="+temperature+", weather="+weather+" WHERE id="+id);
+			} else {
+				stmt.executeUpdate("INSERT INTO conditions VALUES (NULL,"+exercise.getId()+", "+temperature+", "+weather+")");
+				ResultSet rs = stmt.executeQuery("SELECT last_insert_id() FROM conditions");
+				id = rs.getInt(1);
+			}
+		} catch (Exception e) {
+			System.out.println("db error during saving of the condition");
+			return;
+		}
 		
 	}
 
